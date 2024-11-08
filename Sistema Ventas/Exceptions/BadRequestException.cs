1df842868df01c783cbc566b448ff194b687f85f
Peter Osorio <PeterMichaelOsorioSantiago@outlook.com>
2024-11-08 18:52:08 -0500

    Error Middleware Implementation

---
+++ Sistema Ventas/Exceptions/BadRequestException.cs
@@ -0,0 +1,9 @@
+﻿namespace Sistema_Ventas.Exceptions
+{
+    public class BadRequestException:Exception
+    {
+        public BadRequestException(string message):base(message)
+        {         
+        }
+    }
+}
---
+++ Sistema Ventas/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,60 @@
+﻿using Sistema_Ventas.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace Sistema_Ventas.Middleware
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                await HandleExceptionAsync(context, error);
+            }
+        }
+        private Task HandleExceptionAsync(HttpContext context, Exception error)
+        {
+            HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+            var response = new Dictionary<string, string>
+            {
+                {"error",error.Message}
+            };
+            switch (error)
+            {
+                case ArgumentException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    break;
+                case UnauthorizedAccessException:
+                    statusCode = HttpStatusCode.Unauthorized;
+                    break;
+                case KeyNotFoundException:
+                    statusCode = HttpStatusCode.NotFound;
+                    break;
+                case InvalidOperationException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    break;
+                case BadRequestException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    break;
+                default:
+                    statusCode = HttpStatusCode.InternalServerError;
+                    break;
+            }
+            context.Response.StatusCode = (int)statusCode;
+            context.Response.ContentType = "application/json";
+
+            return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+    }
+}
