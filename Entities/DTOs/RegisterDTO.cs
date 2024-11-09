311ebdb49f730cb39bfd79484794cb98139cf726
Peter Osorio <PeterMichaelOsorioSantiago@outlook.com>
2024-11-08 19:05:32 -0500

    DTO pattern entities

---
+++ Entities/DTOs/RegisterDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public class RegisterDTO
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "El correo es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El correo no es valido.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [MinLength(8, ErrorMessage = "La contraseña debe tener minimo 8 caracteres.")]
+        public string Password { get; set; }
+        [Compare("Password",ErrorMessage = "Las contraseñas no coinciden.")]
+        public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "La fecha de nacimiento es obligatoria.")]
+        public string birthdate { get; set; }
+    }
+}
--- Sistema Ventas/Program.cs
+++ Sistema Ventas/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Data.Context;
 using Microsoft.EntityFrameworkCore;
+using Sistema_Ventas.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +11,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Conexion base de datos
 var connectionString = builder.Configuration.GetConnectionString("ConexionSQL");
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 
@@ -26,6 +27,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware<ErrorHandlerMiddleware>();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
