e349db333b3d8f2b33fd8e5aa182bb8c67929e57
Peter Osorio <PeterMichaelOsorioSantiago@outlook.com>
2024-11-08 19:50:12 -0500

    User service implementation

---
+++ Service/Impl/UserService.cs
@@ -0,0 +1,38 @@
+﻿using Data.Repository.Interface;
+using Entities.Models;
+using Service.Interface;
+using Microsoft.AspNetCore.Identity;
+
+namespace Service.Impl
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> _repository;
+        private readonly IPasswordHasher<User> _password;
+        public UserService(IRepository<User> repository,
+            IPasswordHasher<User> password)
+        {
+            _repository = repository;
+            _password = password;
+        }
+        public void Authentication()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdatePassword()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
