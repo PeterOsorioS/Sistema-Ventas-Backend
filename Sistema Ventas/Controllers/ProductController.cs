a164dfe308659b0deb402d32c09de7c304dbf9c6
Peter Osorio <PeterMichaelOsorioSantiago@outlook.com>
2024-11-11 20:12:49 -0500

    Product controller implementation

---
+++ Sistema Ventas/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.Interface;
+
+namespace Sistema_Ventas.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id) 
+        {
+            try
+            {
+                var response = _productService.GetProduct(id);
+                return Ok(response);
+            }
+            catch (BadRequestException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
