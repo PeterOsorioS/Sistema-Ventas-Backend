ed1a50e05249ef267aaf71f2802192a301f399a0
Peter Osorio <PeterMichaelOsorioSantiago@outlook.com>
2024-11-12 12:24:00 -0500

    CategoryDTO creation

---
+++ Service/DTOs/CategoryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.DTOs
+{
+    public class CategoryDTO
+    {
+        [Required(ErrorMessage = "El nombre de la categoria es obligatorio.")]
+        public string Name { get; set; }
+    }
+}
