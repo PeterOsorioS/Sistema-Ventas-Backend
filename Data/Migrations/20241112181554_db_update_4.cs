16bc7fbde3dde34bb127e85f4eb0178452a9ad7a
Peter Osorio <PeterMichaelOsorioSantiago@outlook.com>
2024-11-12 13:24:15 -0500

    Settings changes in the db

---
+++ Data/Migrations/20241112181554_db_update_4.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class db_update_4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "categories",
+                newName: "name");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "state",
+                table: "products",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+
+            migrationBuilder.AddColumn<string>(
+                name: "creation_date",
+                table: "categories",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "state",
+                table: "categories",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_categories_name",
+                table: "categories",
+                column: "name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_users_email",
+                table: "users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_categories_name",
+                table: "categories");
+
+            migrationBuilder.DropColumn(
+                name: "creation_date",
+                table: "categories");
+
+            migrationBuilder.DropColumn(
+                name: "state",
+                table: "categories");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "categories",
+                newName: "Name");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "state",
+                table: "products",
+                type: "boolean",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: true);
+        }
+    }
+}
