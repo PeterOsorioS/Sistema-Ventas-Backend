5af866eda0a517a357dc1b718d1445d5e210b7ed
Peter Osorio <PeterMichaelOsorioSantiago@outlook.com>
2024-11-12 12:32:24 -0500

    Category model update

--- Entities/Models/Category.cs
+++ Entities/Models/Category.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +10,13 @@ namespace Entities.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Column("name")]
         public string Name { get; set; }
+        [Column("state")]
+        public bool State {  get; set; }
+        [Column("creation_date")]
+        public string CreationDate { get; set; }
     }
 }
