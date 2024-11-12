a49ccddb0b7d4e63178280fbd950ab577d85855b
Peter Osorio <PeterMichaelOsorioSantiago@outlook.com>
2024-11-12 12:43:57 -0500

    Adjustments to ProductDTO

--- Service/DTOs/ProductDTO.cs
+++ Service/DTOs/ProductDTO.cs
@@ -10,20 +10,13 @@ namespace Service.DTOs
 {
     public class ProductDTO
     {
-        [Required(ErrorMessage = "El Id es obligatorio.")]
         public int Id { get; set; }
-        [Required(ErrorMessage ="El nombre es obligatorio.")]
         public string Name { get; set; }
         public string? Description { get; set; }
         public string? CodeQR { get; set; }
-        [Required(ErrorMessage = "El precio es obligatorio.")]
         public decimal Price { get; set; }
-        [Required(ErrorMessage = "El stock es obligatorio.")]
-        [Range(0, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a 0")]
         public int Stock { get; set; }
-        [Required(ErrorMessage = "El estado es obligatorio.")]
         public bool State { get; set; }
-        [Required(ErrorMessage = "La categoria es obligatoria.")]
         public int IdCategory { get; set; }
     }
 }
